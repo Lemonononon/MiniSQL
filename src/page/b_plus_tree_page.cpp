413bf69ea51a92dc111eb18b87e79fbb888c9bad
刘奕骁 <liuyixiao0821@gmail.com>
2022-05-06 14:20:09 +0800

    Complete getter and setter, calculate minSize

--- src/page/b_plus_tree_page.cpp
+++ src/page/b_plus_tree_page.cpp
@@ -5,15 +5,15 @@
  * Page type enum class is defined in b_plus_tree_page.h
  */
 bool BPlusTreePage::IsLeafPage() const {
-  return false;
+  return page_type_ == IndexPageType::LEAF_PAGE;
 }
 
 bool BPlusTreePage::IsRootPage() const {
-  return false;
+  return parent_page_id_ == INVALID_PAGE_ID;
 }
 
 void BPlusTreePage::SetPageType(IndexPageType page_type) {
-
+  this->page_type_ = page_type;
 }
 
 /*
@@ -21,26 +21,26 @@ void BPlusTreePage::SetPageType(IndexPageType page_type) {
  * page)
  */
 int BPlusTreePage::GetSize() const {
-  return 0;
+  return size_;
 }
 
 void BPlusTreePage::SetSize(int size) {
-
+  this->size_ = size;
 }
 
 void BPlusTreePage::IncreaseSize(int amount) {
-
+  this->size_ += amount;
 }
 
 /*
  * Helper methods to get/set max size (capacity) of the page
  */
 int BPlusTreePage::GetMaxSize() const {
-  return 0;
+  return max_size_;
 }
 
 void BPlusTreePage::SetMaxSize(int size) {
-
+  max_size_ = size;
 }
 
 /*
@@ -48,29 +48,39 @@ void BPlusTreePage::SetMaxSize(int size) {
  * Generally, min page size == max page size / 2
  */
 int BPlusTreePage::GetMinSize() const {
-  return 0;
+  if (IsRootPage()) {
+    if (IsLeafPage()) {
+      return 1;
+    }
+    else {
+      return 2;
+    }
+  }
+  else {
+    return (max_size_ + 1) / 2;
+  }
 }
 
 /*
  * Helper methods to get/set parent page id
  */
 page_id_t BPlusTreePage::GetParentPageId() const {
-  return INVALID_PAGE_ID;
+  return parent_page_id_;
 }
 
 void BPlusTreePage::SetParentPageId(page_id_t parent_page_id) {
-
+  this->parent_page_id_  = parent_page_id;
 }
 
 /*
  * Helper methods to get/set self page id
  */
 page_id_t BPlusTreePage::GetPageId() const {
-  return INVALID_PAGE_ID;
+  return page_id_;
 }
 
 void BPlusTreePage::SetPageId(page_id_t page_id) {
-
+  this->page_id_ = page_id;
 }
 
 /*
