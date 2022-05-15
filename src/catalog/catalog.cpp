a00caf8c0d86fa3a8b8466cf5ab28a07ee6eb7b8
TomisTony <1158176767@qq.com>
2022-05-15 14:12:33 +0800

    finish serialize in catalogMeta

--- src/catalog/catalog.cpp
+++ src/catalog/catalog.cpp
@@ -1,7 +1,42 @@
 #include "catalog/catalog.h"
 
+/*
+ * 序列化catalog的meta_data,看似不需要像record里面那样返回偏移值
+ * 序列化顺序:
+ * 1.magic_number
+ * 2.table_meta_pages_.size()
+ * 3.index_meta_pages_.size()
+ * 4.table_meta_pages_,每次写入两个byte,分别为table_id_t, page_id_t
+ * 5.index_meta_pages_,每次写入两个byte,分别为index_id_t, page_id_t
+ */
 void CatalogMeta::SerializeTo(char *buf) const {
-  // ASSERT(false, "Not Implemented yet");
+  // write magic_number
+  MACH_WRITE_INT32(buf, CATALOG_METADATA_MAGIC_NUM);
+  buf += 4;
+  // write table_meta_pages_.size()
+  MACH_WRITE_INT32(buf, table_meta_pages_.size());
+  buf += 4;
+  // write index_meta_pages_.size()
+  MACH_WRITE_INT32(buf, index_meta_pages_.size());
+  buf += 4;
+  // write table_meta_pages_
+  auto table_meta_pages_it = table_meta_pages_.begin();
+  while (table_meta_pages_it != table_meta_pages_.end()) {
+    MACH_WRITE_UINT32(buf, table_meta_pages_it->first);
+    buf += 4;
+    MACH_WRITE_INT32(buf, table_meta_pages_it->second);
+    buf += 4;
+    table_meta_pages_it++;
+  }
+  // write index_meta_pages_
+  auto index_meta_pages_it = index_meta_pages_.begin();
+  while (index_meta_pages_it != index_meta_pages_.end()) {
+    MACH_WRITE_UINT32(buf, index_meta_pages_it->first);
+    buf += 4;
+    MACH_WRITE_INT32(buf, index_meta_pages_it->second);
+    buf += 4;
+    index_meta_pages_it++;
+  }
 }
 
 CatalogMeta *CatalogMeta::DeserializeFrom(char *buf, MemHeap *heap) {
@@ -16,20 +51,19 @@ uint32_t CatalogMeta::GetSerializedSize() const {
 
 CatalogMeta::CatalogMeta() {}
 
-
 CatalogManager::CatalogManager(BufferPoolManager *buffer_pool_manager, LockManager *lock_manager,
                                LogManager *log_manager, bool init)
-        : buffer_pool_manager_(buffer_pool_manager), lock_manager_(lock_manager),
-          log_manager_(log_manager), heap_(new SimpleMemHeap()) {
+    : buffer_pool_manager_(buffer_pool_manager),
+      lock_manager_(lock_manager),
+      log_manager_(log_manager),
+      heap_(new SimpleMemHeap()) {
   // ASSERT(false, "Not Implemented yet");
 }
 
-CatalogManager::~CatalogManager() {
-  delete heap_;
-}
+CatalogManager::~CatalogManager() { delete heap_; }
 
-dberr_t CatalogManager::CreateTable(const string &table_name, TableSchema *schema,
-                                    Transaction *txn, TableInfo *&table_info) {
+dberr_t CatalogManager::CreateTable(const string &table_name, TableSchema *schema, Transaction *txn,
+                                    TableInfo *&table_info) {
   // ASSERT(false, "Not Implemented yet");
   return DB_FAILED;
 }
@@ -71,7 +105,6 @@ dberr_t CatalogManager::DropIndex(const string &table_name, const string &index_
   // ASSERT(false, "Not Implemented yet");
   return DB_FAILED;
 }
-
 
 dberr_t CatalogManager::FlushCatalogMetaPage() const {
   // ASSERT(false, "Not Implemented yet");
